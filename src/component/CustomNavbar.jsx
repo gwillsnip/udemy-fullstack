1243ce6f84ce9e7a4454d8908112633c8f24a395
Ben <benyforu2020@gmail.com>
2019-03-26 18:30:25 +0100

    first upload

---
+++ src/component/CustomNavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomNavbar = () => {
+  return (
+    <div>
+      <nav>
+        <h1>new</h1>
+      </nav>
+    </div>
+  );
+};
+export default CustomNavbar;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h2> I am new here</h2>
+      </header>
+    </div>
+  );
+};
+export default Header;
