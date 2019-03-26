2c5893aa14705ac879a4392f63a4c3a5e365e721
Ben <benyforu2020@gmail.com>
2019-03-26 19:07:18 +0100

    app edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './component/CustomNavbar';
-import Footer from './component/Footer';
+import Footer from './pages/Footer';
 class App extends Component {
   render() {
     return (
