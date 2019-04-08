02ffa638a291bf879b3e854fda0f4ec3abb26bf0
ben <benyforu2020@gmail.com>
2019-04-08 23:35:22 +0200

    clean up the code

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render( < App / > , document.getElementById('root')); {
-   /* if (module.hot) {
-   module.hot.accept();
-} */
+   if (module.hot) {
+      module.hot.accept();
+   }
 }
 registerServiceWorker();
