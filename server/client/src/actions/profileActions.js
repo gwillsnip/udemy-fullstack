b64b55db862cf4f429c04f6b40a1e9038307af7f
ben <benyforu2020@gmail.com>
2019-04-08 22:45:31 +0200

    created profile by handle and sub components

--- server/client/src/actions/profileActions.js
+++ server/client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
    PROFILE_LOADING,
    CLEAR_CURRENT_PROFILE,
    GET_ERRORS,
-   SET_CURRENT_USER
+   SET_CURRENT_USER,
+   GET_PROFILES
 } from './types';
 
 // Get current profile
@@ -13,6 +14,25 @@ export const getCurrentProfile = () => dispatch => {
    dispatch(setProfileLoading());
    axios
       .get('/api/profile')
+      .then(res =>
+         dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+         })
+      )
+      .catch(err =>
+         dispatch({
+            type: GET_PROFILE,
+            payload: {}
+         })
+      );
+};
+
+// Get get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+   dispatch(setProfileLoading());
+   axios
+      .get(`/api/profile/handle/${handle}`)
       .then(res =>
          dispatch({
             type: GET_PROFILE,
@@ -102,6 +122,24 @@ export const deleteEducation = id => dispatch => {
       );
 };
 
+//Get all Profiles
+export const getProfiles = () => dispatch => {
+   dispatch(setProfileLoading());
+   axios
+      .get('/api/profile/all')
+      .then(res =>
+         dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+         })
+      )
+      .catch(err =>
+         dispatch({
+            type: GET_PROFILES,
+            payload: null
+         })
+      );
+};
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
    if (window.confirm('Are you sure? This can NOT be undone!')) {
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
-if (module.hot) {
+ReactDOM.render( < App / > , document.getElementById('root')); {
+   /* if (module.hot) {
    module.hot.accept();
+} */
 }
 registerServiceWorker();
