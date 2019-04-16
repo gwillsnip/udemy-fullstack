0eaf5d27045bdba78d9e86650b8d2393feb06095
Ben <benyforu2020@gmail.com>
2019-04-16 19:23:27 +0200

    completed Profile and touch ups

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( < App / > , document.getElementById('root')); {
-   if (module.hot) {
-      module.hot.accept();
-   }
-}
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+
 registerServiceWorker();
